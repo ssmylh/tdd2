a4ab1dfc81579e8dd96e3a50fd92dbe181a0cd81
ssmylh <ssmylh@gmail.com>
2018-01-23 11:49:12 +0900

    Chapter 23

--- xunit.py
+++ xunit.py
@@ -5,8 +5,7 @@ class TestCase:
         pass
     def tearDown(self):
         pass
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.testStarted()
         self.setUp()
         try:
@@ -15,7 +14,6 @@ class TestCase:
         except:
             result.testFailed()
         self.tearDown()
-        return result
 
 class TestResult:
     def __init__(self):
@@ -28,6 +26,15 @@ class TestResult:
     def summary(self):
         return "%d run, %d failed" % (self.runCount, self.errorCount)
 
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+    def add(self, test):
+        self.tests.append(test)
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
@@ -39,29 +46,43 @@ class WasRun(TestCase):
         self.log += "tearDown "
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert("setUp testMethod tearDown " == test.log)
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
     def testFailedResultFormatting(self):# For TestResult
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
     def error(self):
+        self.result.testStarted()
         raise Exception
 
+
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+# suite.add(TestCaseTest("error"))
+result = TestResult()
+suite.run(result)
 # テストの実行結果(特にエラー)は `print` でないと確認出来ない。
-print(TestCaseTest("testTemplateMethod").run().summary())
-print(TestCaseTest("testResult").run().summary())
-print(TestCaseTest("testFailedResult").run().summary())
-print(TestCaseTest("testFailedResultFormatting").run().summary())
-print(TestCaseTest("error").run().summary())
+print(result.summary())
